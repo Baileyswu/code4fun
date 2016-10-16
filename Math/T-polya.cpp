216098f7a0689550a7e3a90d5266a1e80c0bf31c
Starry Tian <starrytian1106@gmail.com>
2016-10-16 18:35:14 +0800

    Update T-polya.cpp

--- Math/T-polya.cpp
+++ Math/T-polya.cpp
@@ -1,75 +1,65 @@
-#include<cmath>
-#include<cstdio>
 #include<iostream>
 using namespace std;
-
-const int N=40000;
-
-int n,P;
-int prim[N],p_num;
-bool flag[N];
-
-void prime()//用筛法求质数 
-{int i,j;
- memset(flag,true,sizeof(flag));
- for(i=2;i<N;i++)
-   if(flag[i])
-     for(j=i*i;j<N;j+=i)
-       flag[j] = false;
- p_num = 0;
- for(i=2;i<N;i++)
-   if(flag[i]) prim[++p_num]=i;
+int n,m,ans;
+void init(void)
+{
+	freopen("bead.in","r",stdin);
+	freopen("bead.out","w",stdout);
 }
-
-int phi(int x)//求欧拉函数
-{int i,j;
- int num=x;
- for(i=1;prim[i]*prim[i]<=x;i++)
-   if(x%prim[i]==0)
-     {num=(num/prim[i])*(prim[i]-1);
-      while (x%prim[i]==0)
-        x=x/prim[i];
-     }
- if(x!=1) num=(num/x)*(x-1);
- return num%P;
+int gcd(int x,int y)
+{
+	if(y==0)
+		return x;
+	return gcd(y,x%y);
 }
-
-int exp(int x, int y)//求x的y次方mod p
-{long long i;
- if (y==0) return 1;
- if (y==1) return x%P;
- i=exp(x,y/2);
- i=(i*i)%P;
- if (y%2==1) i=i*x;
- return i%P;
+int ksm(int x,int y)
+{
+	int i,j=x,k=1;
+	while(y)
+	{
+		if(y%2)
+			k=k*j;
+		j=j*j;
+		y/=2;
+	}
+	return k;
 }
-
-void work()
-{int i,j,k;
- int ans=0;
- for (i=1;i*i<=n;i++)//枚举长度
-   if (i*i==n)
-     {ans+=phi(i)*exp(n,i-1);
-      ans=ans%P;
-     }
-   else
-     if (n%i==0) //有长度为i的循环,就会有长度为n/i的循环
-       {ans+=phi(i)*exp(n,n/i-1);//枚举循环长度i,找出相应的i的个数:gcd(n,i)=n/i
-        ans+=phi(n/i)*exp(n,i-1);
-        ans=ans%P;
-       }
- cout<<ans<<endl;
-} 
-
-int main()
-{freopen("color.in","r",stdin);
- freopen("color.out","w",stdout);
- int cases;
- scanf("%d",&cases);
- prime();
- while(cases--)
-   {scanf("%d%d",&n,&P);
-    work(); 
-   }
- return 0;
+void work(void)
+{
+	int i;
+	ans=0;
+	for(i=1;i<=m;i++)
+	{
+		//cout<<n<<' '<<gcd(i,m)<<endl;
+		ans+=ksm(n,gcd(i,m));
+	}
+	//cout<<ans<<endl;
+	
+//	cout<<ans<<endl;
+	if(m%2==0)
+	{
+		ans+=m/2*ksm(n,(m+1)/2);
+		ans+=m/2*ksm(n,m/2+1);
+		//cout<<ans<<endl;
+		ans/=m*2;
+	}
+	else
+	{
+		ans+=m*ksm(n,(m+1)/2);
+		//cout<<ans<<endl;
+		ans/=m*2;
+	}
+	//cout<<ans<<endl;
+	cout<<ans<<endl;
+}
+int main(void)
+{
+	init();
+	cin>>n>>m;
+	while(n||m)
+	{
+		work();
+		cin>>n>>m;
+	}
+	return 0;
 }
